d7b2a1b6a2caf17ca6e34a6e00993f4ee884da9f
wolflergf <54420326+wolflergf@users.noreply.github.com>
2023-03-11 10:26:41 +0000

    Incorporate a header.

--- WaterMarkApp.py
+++ WaterMarkApp.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """ Short description of this Python module.
 Longer description of this module.
 This program is free software: you can redistribute it and/or modify it under
