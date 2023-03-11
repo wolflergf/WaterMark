c21c11e63bcd7ab1ebefc0c9d139c53bda8804e2
wolflergf <54420326+wolflergf@users.noreply.github.com>
2023-03-11 10:25:59 +0000

    Incorporate a header.

--- WaterMarkApp.py
+++ WaterMarkApp.py
@@ -1,3 +1,29 @@
+#!/usr/bin/env python
+""" Short description of this Python module.
+Longer description of this module.
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+__author__ = "Wolfler Guzzo Ferreira"
+__contact__ = "wolflerpython@gmail.com"
+__copyright__ = "Copyright 2023"
+__credits__ = ["Wolfler Guzzo Ferreira"]
+__date__ = "2023/03/10"
+__deprecated__ = False
+__email__ = "wolflerpython@gmail.com"
+__license__ = "GPLv3"
+__maintainer__ = "developer"
+__status__ = "Production"
+__version__ = "0.0.1"
+
 from tkinter import *
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 
